e17a12a631be6b67fcfa73fd4a62507821c47c24
Chiffa <artemy040404@gmail.com>
2025-08-18 17:47:24 +0300

    chore: Add tests for calculations with mods

--- osu-native/src/calculator/mania.rs
+++ osu-native/src/calculator/mania.rs
@@ -118,6 +118,10 @@ impl HasNative for ManiaDifficultyAttributes {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
+    use rosu_mods::{Acronym, GameModSimple};
+
     use crate::{
         beatmap::Beatmap,
         calculator::{DifficultyCalculator, mania::ManiaDifficultyCalculator},
@@ -133,5 +137,25 @@ mod tests {
         let attributes = calculator.calculate().unwrap();
         assert_ne!(attributes.star_rating, 0.0);
         assert_eq!(attributes.max_combo, 1463);
+    }
+    #[test]
+    fn test_toy_box_mania_with_mods() {
+        let beatmap = Beatmap::from_path(initialize_path()).unwrap();
+        let ruleset = Ruleset::new(RulesetKind::Mania).unwrap();
+        let calculator = ManiaDifficultyCalculator::new(ruleset, &beatmap).unwrap();
+        let attributes = calculator.calculate().unwrap();
+        let ruleset = Ruleset::new(RulesetKind::Mania).unwrap();
+        let mods: GameModSimple = GameModSimple {
+            acronym: Acronym::from_str("DT").unwrap(),
+            settings: Default::default(),
+        };
+        let calculator_with_mods = ManiaDifficultyCalculator::new(ruleset, &beatmap)
+            .unwrap()
+            .mods(vec![mods])
+            .unwrap();
+        let attributes_with_mods = calculator_with_mods.calculate().unwrap();
+
+        assert!(attributes_with_mods.star_rating > attributes.star_rating);
+        assert!(attributes_with_mods.max_combo == attributes.max_combo);
     }
 }
