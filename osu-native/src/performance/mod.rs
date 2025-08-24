0e88cb1d1cc5da8ffc880b5a706a9ed13c7fb77a
Chiffa <artemy040404@gmail.com>
2025-08-24 18:52:04 +0300

    feat: Add initial support for typestate-driven builder

--- osu-native/src/performance/mod.rs
+++ osu-native/src/performance/mod.rs
@@ -73,3 +73,259 @@ impl Default for ScoreStatistics {
         }
     }
 }
+
+pub struct Empty;
+pub struct WithBeatmap;
+
+pub struct Osu;
+pub struct Taiko;
+pub trait RulesetTrait {
+    // type DifficultyAttributes;
+    type PerformanceCalculatorTy: PerformanceCalculator;
+}
+impl RulesetTrait for Osu {
+    // type DifficultyAttributes = OsuDifficultyAttributes;
+
+    type PerformanceCalculatorTy = OsuPerformanceCalculator;
+}
+impl RulesetTrait for Taiko {
+    // type DifficultyAttributes = TaikoDifficultyAttributes;
+
+    type PerformanceCalculatorTy = TaikoPerformanceCalculator;
+}
+pub struct WithRuleset<T: RulesetTrait> {
+    _marker: PhantomData<T>,
+}
+
+pub struct PerformanceCalculatorBuilder<T: RulesetTrait> {
+    beatmap: Beatmap,
+    ruleset: Ruleset,
+    mods: Vec<GameModSimple>,
+    difficulty_attributes: <<T as RulesetTrait>::PerformanceCalculatorTy as PerformanceCalculator>::DifficultyAttributes,
+    score_state: ScoreStatistics,
+    _marker: PhantomData<T>,
+}
+
+pub struct CalculatorBuilder<T> {
+    beatmap: Option<Beatmap>,
+    ruleset: Option<Ruleset>,
+    mods: Option<Vec<GameModSimple>>,
+    _marker: PhantomData<T>,
+}
+
+impl CalculatorBuilder<Empty> {
+    pub fn new() -> CalculatorBuilder<Empty> {
+        CalculatorBuilder::<Empty> {
+            beatmap: None,
+            ruleset: None,
+            mods: None,
+            _marker: PhantomData::<Empty>,
+        }
+    }
+}
+
+impl CalculatorBuilder<Empty> {
+    pub fn with_beatmap(
+        self,
+        map: impl AsRef<Path>,
+    ) -> Result<CalculatorBuilder<WithBeatmap>, BeatmapError> {
+        let beatmap = Beatmap::from_path(map)?;
+        Ok(CalculatorBuilder {
+            beatmap: Some(beatmap),
+            ruleset: None,
+            mods: None,
+            _marker: PhantomData::<WithBeatmap>,
+        })
+    }
+}
+
+impl CalculatorBuilder<WithBeatmap> {
+    fn osu(self) -> Result<CalculatorBuilder<WithRuleset<Osu>>, RulesetError> {
+        let ruleset = Ruleset::new(RulesetKind::Osu)?;
+        Ok(CalculatorBuilder {
+            beatmap: self.beatmap,
+            ruleset: Some(ruleset),
+            mods: None,
+            _marker: PhantomData::<WithRuleset<Osu>>,
+        })
+    }
+    fn taiko(self) -> Result<CalculatorBuilder<WithRuleset<Taiko>>, RulesetError> {
+        let ruleset = Ruleset::new(RulesetKind::Taiko)?;
+        Ok(CalculatorBuilder {
+            beatmap: self.beatmap,
+            ruleset: Some(ruleset),
+            mods: None,
+            _marker: PhantomData::<WithRuleset<Taiko>>,
+        })
+    }
+    // fn mania(self) -> Result<CalculatorBuilder<WithRuleset<Mania>>, RulesetError> {
+    //     todo!()
+    // }
+    // fn catch(self) -> Result<CalculatorBuilder<WithRuleset<Catch>>, RulesetError> {
+    //     todo!()
+    // }
+}
+
+impl<T> CalculatorBuilder<WithRuleset<T>>
+where
+    T: RulesetTrait,
+{
+    fn mods(
+        mut self,
+        mods: impl IntoGameMods,
+    ) -> Result<CalculatorBuilder<WithRuleset<T>>, ModCollectionError> {
+        self.mods = Some(mods.into_mods()?.0);
+        Ok(self)
+    }
+}
+
+trait Difficulty {
+    type Calculator: DifficultyCalculator;
+    type RulesetType: RulesetTrait;
+    fn difficulty(self) -> Result<Self::Calculator, OsuError>;
+    fn performance(self) -> Result<PerformanceCalculatorBuilder<Self::RulesetType>, OsuError>;
+}
+impl Difficulty for CalculatorBuilder<WithRuleset<Osu>> {
+    type Calculator = OsuDifficultyCalculator;
+    type RulesetType = Osu;
+    fn difficulty(self) -> Result<Self::Calculator, OsuError> {
+        Self::Calculator::new(self.ruleset.unwrap(), self.beatmap.as_ref().unwrap())
+    }
+    fn performance(self) -> Result<PerformanceCalculatorBuilder<Self::RulesetType>, OsuError> {
+        let ruleset = Ruleset::new(self.ruleset.as_ref().unwrap().kind).unwrap();
+        let attr = Self::Calculator::new(self.ruleset.unwrap(), self.beatmap.as_ref().unwrap())?
+            .calculate()?;
+        let mut score = ScoreStatistics::default();
+        score.max_combo = attr.max_combo;
+        Ok(PerformanceCalculatorBuilder {
+            beatmap: self.beatmap.unwrap(),
+            ruleset: ruleset,
+            mods: self.mods.unwrap(),
+            difficulty_attributes: attr,
+            score_state: score,
+            _marker: PhantomData::<Osu>,
+        })
+    }
+}
+
+impl Difficulty for CalculatorBuilder<WithRuleset<Taiko>> {
+    type Calculator = TaikoDifficultyCalculator;
+    fn difficulty(self) -> Result<Self::Calculator, OsuError> {
+        Self::Calculator::new(self.ruleset.unwrap(), self.beatmap.as_ref().unwrap())
+    }
+
+    type RulesetType = Taiko;
+
+    fn performance(self) -> Result<PerformanceCalculatorBuilder<Self::RulesetType>, OsuError> {
+        let ruleset = Ruleset::new(self.ruleset.as_ref().unwrap().kind).unwrap();
+        let attr = Self::Calculator::new(self.ruleset.unwrap(), self.beatmap.as_ref().unwrap())?
+            .calculate()?;
+        let mut score = ScoreStatistics::default();
+        score.max_combo = attr.max_combo;
+        Ok(PerformanceCalculatorBuilder {
+            beatmap: self.beatmap.unwrap(),
+            ruleset: ruleset,
+            mods: self.mods.unwrap(),
+            difficulty_attributes: attr,
+            score_state: score,
+            _marker: PhantomData::<Taiko>,
+        })
+    }
+}
+
+impl<T: RulesetTrait> PerformanceCalculatorBuilder<T> {
+    fn with_score_state(mut self, score: ScoreStatistics) -> Self {
+        self.score_state = score;
+        self
+    }
+    fn max_combo(mut self, n: i32) -> Self {
+        self.score_state.max_combo = n;
+        self
+    }
+    fn misses(mut self, n: i32) -> Self {
+        self.score_state.count_miss = n;
+        self
+    }
+    fn accuracy(mut self, acc: f64) -> Self {
+        self.score_state.accuracy = acc;
+        self
+    }
+    fn calculator(
+        self,
+    ) -> Result<<T::PerformanceCalculatorTy as PerformanceCalculator>::Attributes, OsuError> {
+        let calc = T::PerformanceCalculatorTy::new().unwrap().calculate(
+            &self.ruleset,
+            &self.score_state,
+            &self.beatmap,
+            self.mods,
+            &self.difficulty_attributes,
+        )?;
+        Ok(calc)
+    }
+}
+
+impl PerformanceCalculatorBuilder<Osu> {
+    fn n300(mut self, n: i32) -> Self {
+        self.score_state.count_great = n;
+        self
+    }
+    fn n100(mut self, n: i32) -> Self {
+        self.score_state.count_ok = n;
+        self
+    }
+    fn n50(mut self, n: i32) -> Self {
+        self.score_state.count_meh = n;
+        self
+    }
+    fn slider_tick_hits(mut self, n: i32) -> Self {
+        self.score_state.count_slider_tail_hit = n;
+        self
+    }
+    fn slider_tick_misses(mut self, n: i32) -> Self {
+        self.score_state.count_slider_tail_hit = self.difficulty_attributes.slider_count - n;
+        self
+    }
+}
+
+impl PerformanceCalculatorBuilder<Taiko> {
+    fn n300(mut self, n: i32) -> Self {
+        self.score_state.count_great = n;
+        self
+    }
+
+    fn n100(mut self, n: i32) -> Self {
+        self.score_state.count_ok = n;
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        difficulty::DifficultyCalculator,
+        performance::{CalculatorBuilder, Difficulty},
+        utils::initialize_path,
+    };
+
+    #[test]
+    fn test_typestate() -> Result<(), Box<dyn std::error::Error>> {
+        let calc = CalculatorBuilder::new()
+            .with_beatmap(initialize_path())?
+            .osu()?
+            .mods(vec![])?
+            .difficulty()?
+            .calculate()?;
+        assert!(calc.star_rating > 0.0);
+        assert_eq!(calc.max_combo, 719);
+        let calc = CalculatorBuilder::new()
+            .with_beatmap(initialize_path())?
+            .osu()?
+            .mods(vec![])?
+            .performance()?
+            .max_combo(250)
+            .n100(10)
+            .accuracy(0.98)
+            .calculator()?;
+        Ok(())
+    }
+}
